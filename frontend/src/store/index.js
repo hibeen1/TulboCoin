d4aa96a7cbe8c021fb5c662498705c8526be5a82
SSAFY <qownstlr0394@naver.com>
2022-09-30 14:50:30 +0900

    test : 코인별 신문 기능 테스트

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -9,6 +9,7 @@ import { coinSaga } from "./coinSaga";
 let BASE_URL = "https://j7e203.p.ssafy.io/api/";
 if (process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:8080/api/";
+  // BASE_URL = "http://localhost:3000/";
 }
 
 const DEFAULT_ACCEPT_TYPE = "application/json";
