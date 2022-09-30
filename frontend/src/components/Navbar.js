b86648d815a6d2d9066806b5fb5fdc8493e35f4b
SSAFY <qownstlr0394@naver.com>
2022-09-30 16:17:50 +0900

    fix : 네브바 링크 수정

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,33 +5,29 @@ import { logoutAsync } from "../store/accountSaga";
 import classes from "./Navbar.module.css";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-
 const NavBlock = styled.div`
-width: 10vw;
-height: 100vh;
-border: 3px blue solid;
-position: fixed;
-/* top: 0; */
-flex-direction: column;
-margin-left: 2vw;
-margin-top: 3vh;
-align-items: center;
-justify-content: space-between;
-
-.Link{
+  width: 10vw;
+  height: 100vh;
+  border: 3px blue solid;
+  position: fixed;
+  /* top: 0; */
+  flex-direction: column;
+  margin-left: 2vw;
   margin-top: 3vh;
-}
+  align-items: center;
+  justify-content: space-between;
 
-` 
+  .Link {
+    margin-top: 3vh;
+  }
+`;
 
 const LogoBlock = styled.div`
   width: 5vw;
   height: 7vh;
   background-image: url(${Logo});
   background-size: 5vw 7vh;
-`
-
-
+`;
 
 function Navbar() {
   // useSelector는 리덕스 스토어의 상태를 조회하는 Hook입니다.
@@ -39,50 +35,44 @@ function Navbar() {
   const { isLoggedin } = useSelector((state) => ({
     isLoggedin: state.account.isLoggedin,
   }));
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // useDispatch 는 리덕스 스토어의 dispatch 를 함수에서 사용 할 수 있게 해주는 Hook 입니다.
   const dispatch = useDispatch();
   // 각 액션들을 디스패치하는 함수들을 만드세요
   const onLogout = () => {
     dispatch(logoutAsync());
-    navigate('/')
-  }
+    navigate("/");
+  };
   return (
     <>
-    <NavBlock>
+      <NavBlock>
         <Link to="/">
           <LogoBlock></LogoBlock>
           {/* <img style={{ width: "5vw", height: "7vh" }} src={Logo} alt="" /> */}
         </Link>
         <br />
-            <Link to="sise">시세</Link>
+        <Link to="/sise">시세</Link>
+        {/* <br /> */}
+        {/* <Link to="/info">정보집합소</Link> */}
+        <br />
+        <Link to="/honor/*">명예의전당</Link>
+        <br />
+
+        {isLoggedin ? (
+          <>
+            <Link to="/mypage">마이페이지</Link>
             <br />
-            <Link to="info">정보집합소</Link>
-            <br />
-            <Link to="honor/*">명예의전당</Link>
-            <br />
 
-          {isLoggedin ? (
-            <>
-            
-                <Link to="mypage">마이페이지</Link>
-                <br />
-              
-            
-                <button onClick={onLogout}>로그아웃</button>
-              
-            </>
-          ) : (
-            <>
+            <button onClick={onLogout}>로그아웃</button>
+          </>
+        ) : (
+          <>
+            <Link to="/signup">회원가입</Link>
 
-                <Link to="signup">회원가입</Link>
-
-
-                <Link to="login">로그인</Link>
-
-            </>
-          )}
+            <Link to="/login">로그인</Link>
+          </>
+        )}
       </NavBlock>
     </>
   );
