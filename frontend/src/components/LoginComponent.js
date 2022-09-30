f9a6784bb064c08ea0b6c2d45b75a47b6e0f736e
SSAFY <qownstlr0394@naver.com>
2022-09-30 16:01:56 +0900

    fix : 회원가입 버튼 수정

--- frontend/src/components/LoginComponent.js
+++ frontend/src/components/LoginComponent.js
@@ -1,39 +1,36 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { loginAsync } from '../store/accountSaga';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-
+import React from "react";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { loginAsync } from "../store/accountSaga";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const LoginPageBlock = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-width: 30vw;
-height: 5vh;
-flex-direction: column;
-margin-bottom: 5vh;
-/* border: 3px solid black; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30vw;
+  height: 5vh;
+  flex-direction: column;
+  margin-bottom: 5vh;
+  /* border: 3px solid black; */
 
-h1{
-  font-size: 8vmin;
-}
-  
-`
+  h1 {
+    font-size: 8vmin;
+  }
+`;
 
 const StyledInput = styled.input`
-border-radius: 40px;
-height: 5vh;
-border: 1px solid #7A7A7A;
-margin-top: 0.5vh;
-margin-bottom: 1vh;
-::placeholder {
-    font-size: 1.2rem;    
-    color: #7A7A7A;
+  border-radius: 40px;
+  height: 5vh;
+  border: 1px solid #7a7a7a;
+  margin-top: 0.5vh;
+  margin-bottom: 1vh;
+  ::placeholder {
+    font-size: 1.2rem;
+    color: #7a7a7a;
   }
-
-
-`
+`;
 // 떠나볼까요
 const StyledButton = styled.button`
   border-radius: 40px;
@@ -42,101 +39,108 @@ const StyledButton = styled.button`
   font-size: 3vmin;
   padding-bottom: 0;
   margin-top: 4vh;
-  
+
   color: #ffffff;
-  background-color: #3A53C0;
+  background-color: #3a53c0;
   cursor: pointer;
   &:hover {
     transform: scale(1.1);
   }
-`
+`;
 
 const Styledlabel = styled.label`
   font-size: 2vmin;
   color: #697ed9;
   margin-left: 10px;
   margin-bottom: 10px;
-`
+`;
 
 const StyledContext = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-flex-direction: column;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 // 아직 계정이 없나요
 const StyledText = styled.div`
-margin-top: 10vh;
-font-size: 3vmin;
-color: #7A7A7A;
-cursor: pointer;
+  margin-top: 10vh;
+  font-size: 3vmin;
+  color: #7a7a7a;
+  cursor: pointer;
   &:hover {
     transform: scale(1.1);
   }
-
 `;
-
-
-
 
 function LoginComponent() {
   // useSelector는 리덕스 스토어의 상태를 조회하는 Hook입니다.
   // state의 값은 store.getState() 함수를 호출했을 때 나타나는 결과물과 동일합니다.
-  const isLoggedin = useSelector(state => state.account.isLoggedin);
+  const isLoggedin = useSelector((state) => state.account.isLoggedin);
 
   // useDispatch 는 리덕스 스토어의 dispatch 를 함수에서 사용 할 수 있게 해주는 Hook 입니다.
   const dispatch = useDispatch();
   // 각 액션들을 디스패치하는 함수들을 만드세요
   // const onLogin = () => dispatch(login());
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   useEffect(() => {
     if (isLoggedin) {
-      navigate('/', true)
+      navigate("/", true);
     }
-  }, [])
-  
-  useEffect(() => {
-    if (isLoggedin) {
-      navigate('/', true)
-    }
-  }, [isLoggedin])
+  }, []);
 
-  const [ loginForm, setLoginForm ] = useState({
-    userId: '',
-    password: '',
-  })
+  useEffect(() => {
+    if (isLoggedin) {
+      navigate("/", true);
+    }
+  }, [isLoggedin]);
+
+  const [loginForm, setLoginForm] = useState({
+    userId: "",
+    password: "",
+  });
   const handleOnChange = (e) => {
     setLoginForm({
       ...loginForm,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const handleOnSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     dispatch(loginAsync(loginForm));
-  }
-  return <>
-  <LoginPageBlock>
-    <h1>TULBO COIN</h1>
-    <form onSubmit={handleOnSubmit}>
-      <StyledContext>
-        <div>
-        <Styledlabel htmlFor="userId">아이디</Styledlabel>
-        <br></br>
-        <StyledInput type="text" name="userId" id="userId" onChange={handleOnChange} /><br />
-        <Styledlabel htmlFor="password">비밀번호</Styledlabel>
-        <br></br>
-        <StyledInput type="password" name="password" id="password" onChange={handleOnChange} /><br />
-        <br></br>
-        </div>
-        <StyledText>아직 계정이 없나요?</StyledText>
+  };
+  return (
+    <>
+      <LoginPageBlock>
+        <h1>TULBO COIN</h1>
+        <form onSubmit={handleOnSubmit}>
+          <StyledContext>
+            <div>
+              <Styledlabel htmlFor="userId">아이디</Styledlabel>
+              <br></br>
+              <StyledInput type="text" name="userId" id="userId" onChange={handleOnChange} />
+              <br />
+              <Styledlabel htmlFor="password">비밀번호</Styledlabel>
+              <br></br>
+              <StyledInput
+                type="password"
+                name="password"
+                id="password"
+                onChange={handleOnChange}
+              />
+              <br />
+              <br></br>
+            </div>
+            <StyledText>
+              <Link to="/signup">아직 계정이 없나요?</Link>
+            </StyledText>
 
-        <StyledButton>떠나볼까요?</StyledButton>
-      </StyledContext>
-    </form>
-  </LoginPageBlock>
-  </>
+            <StyledButton>떠나볼까요?</StyledButton>
+          </StyledContext>
+        </form>
+      </LoginPageBlock>
+    </>
+  );
 }
 
 export default LoginComponent;
--- frontend/src/store/accountSaga.js
+++ frontend/src/store/accountSaga.js
@@ -2,94 +2,93 @@
 // put : 특정 액션 디스패치
 // takeEvery : 특정 액션 타입에 대하여 디스패치되는 모든 액션들을 처리
 // takeLatest : 특정 액션 타입에 대하여 디스패치된 가장 마지막 액션만을 처리
-import { call, delay, put, takeLatest } from 'redux-saga/effects';
-import { signupApi, fetchUserApi, loginApi, putUserApi, deleteApi, fetchWalletApi } from './api'
-import { logout, fetchUser, changeIsLoggedIn, token, fetchWallet } from './account';
+import { call, delay, put, takeLatest } from "redux-saga/effects";
+import { signupApi, fetchUserApi, loginApi, putUserApi, deleteApi, fetchWalletApi } from "./api";
+import { logout, fetchUser, changeIsLoggedIn, token, fetchWallet } from "./account";
 
 // 액션의 타입
-const CATCH_LOGIN = 'CATCH_LOGIN' // token이 있는지 확인해서 로그인 여부 변경
-const LOGIN_ASYNC = 'LOGIN_ASYNC';
-const LOGOUT_ASYNC = 'LOGOUT_ASYNC'
-const SIGNUP_ASYNC = 'SIGNUP_ASYNC'
-const PUT_USER_ASYNC = 'PUT_USER_ASYNC' // 유저 정보 수정
-const FETCH_USER_ASYNC = 'FETCH_USER_ASYNC'
-const DELETE_USER_ASYNC = 'DELETE_USER_ASYNC' // 회원탈퇴
-const FETCH_WALLET_ASYNC = 'FETCH_WALLET_ASYNC' // 지갑정보 가져오기
-
+const CATCH_LOGIN = "CATCH_LOGIN"; // token이 있는지 확인해서 로그인 여부 변경
+const LOGIN_ASYNC = "LOGIN_ASYNC";
+const LOGOUT_ASYNC = "LOGOUT_ASYNC";
+const SIGNUP_ASYNC = "SIGNUP_ASYNC";
+const PUT_USER_ASYNC = "PUT_USER_ASYNC"; // 유저 정보 수정
+const FETCH_USER_ASYNC = "FETCH_USER_ASYNC";
+const DELETE_USER_ASYNC = "DELETE_USER_ASYNC"; // 회원탈퇴
+const FETCH_WALLET_ASYNC = "FETCH_WALLET_ASYNC"; // 지갑정보 가져오기
 
 // 액션 생성 함수 만들기
 export const loginAsync = (form) => ({ type: LOGIN_ASYNC, meta: form });
-export const logoutAsync = () => ({ type: LOGOUT_ASYNC })
-export const signupAsync = body => ({ type: SIGNUP_ASYNC, meta: body })
-export const catchLogin = () => ({ type: CATCH_LOGIN })
-export const putUserAsync = (form) => ({ type: PUT_USER_ASYNC, meta: form })
-export const fetchUserAsync = () => ({ type: FETCH_USER_ASYNC })
-export const deleteUserAsync = () => ({ type: DELETE_USER_ASYNC })
-export const fetchWalletAsync = () => ({ type: FETCH_WALLET_ASYNC })
+export const logoutAsync = () => ({ type: LOGOUT_ASYNC });
+export const signupAsync = (body) => ({ type: SIGNUP_ASYNC, meta: body });
+export const catchLogin = () => ({ type: CATCH_LOGIN });
+export const putUserAsync = (form) => ({ type: PUT_USER_ASYNC, meta: form });
+export const fetchUserAsync = () => ({ type: FETCH_USER_ASYNC });
+export const deleteUserAsync = () => ({ type: DELETE_USER_ASYNC });
+export const fetchWalletAsync = () => ({ type: FETCH_WALLET_ASYNC });
 
 // 로그인 되었는지 확인
 function* catchLoginSaga() {
   if (localStorage.token !== undefined) {
-    yield put(changeIsLoggedIn(true))
+    yield put(changeIsLoggedIn(true));
   } else {
-    yield put(changeIsLoggedIn(false))
+    yield put(changeIsLoggedIn(false));
   }
 }
 // 로그인 되었는지 확인 끝
 
 // 로그인 시작
 function* loginSaga(action) {
-  const body = action.meta
-  try{
-    const response = yield call(loginApi, body)
+  const body = action.meta;
+  try {
+    const response = yield call(loginApi, body);
     if (response.status === 200) {
-      yield put(token(response.data))
+      yield put(token(response.data));
       yield put(fetchUser(response.data)); // put은 특정 액션을 디스패치 해줍니다.
-      yield put(fetchWalletAsync())
-      yield delay(window.location.replace('/sise'), 1000)
+      yield put(fetchWalletAsync());
+      yield delay((window.location.href = "/sise"), 1000);
     }
   } catch (error) {
-    alert(error.response.data.message)
+    alert(error.response.data.message);
     // console.log(error.data.message)
   }
-  yield put(catchLogin())
+  yield put(catchLogin());
 }
 // 로그인 끝
 
 // 로그아웃
 function* logoutSaga() {
-  yield put(logout())
-  yield put(catchLogin())
+  yield put(logout());
+  yield put(catchLogin());
 }
 // 로그아웃 끝
 
 // 회원가입 시작시키기
 function* signupSaga(action) {
-  const body = action.meta
-  try{
-    const response = yield call(signupApi, body)
+  const body = action.meta;
+  try {
+    const response = yield call(signupApi, body);
     if (response.status === 200) {
-      alert('회원가입 성공')
+      alert("회원가입 성공");
     }
   } catch (error) {
-    console.log(error)
-    alert(error.response.data.message)
+    console.log(error);
+    alert(error.response.data.message);
   }
 }
 // 회원가입 끝
 
 // 내 정보 수정
 function* putUserSaga(action) {
-  const body = action.meta
-  console.log(body)
-  try{
-    const response = yield call(putUserApi, body)
+  const body = action.meta;
+  console.log(body);
+  try {
+    const response = yield call(putUserApi, body);
     if (response.status === 200) {
-      yield put(fetchUserAsync())
-      yield delay(window.location.reload(), 1000)
+      yield put(fetchUserAsync());
+      yield delay(window.location.reload(), 1000);
     }
   } catch (error) {
-    alert(error.response.data.message)
+    alert(error.response.data.message);
     // console.log(error)
   }
 }
@@ -97,46 +96,46 @@ function* putUserSaga(action) {
 
 // 내 정보 받아오기
 function* fetchUserSaga() {
-  console.log('fetchUserAsync가 작동')
-  try{
-    const response = yield call(fetchUserApi)
+  console.log("fetchUserAsync가 작동");
+  try {
+    const response = yield call(fetchUserApi);
     if (response.status === 200) {
-      yield put(fetchUser(response.data))
+      yield put(fetchUser(response.data));
     }
   } catch (error) {
-    alert(error.response.data.message)
+    alert(error.response.data.message);
   }
 }
 // 내 정보 받아오기 끝
 
 // 회원탈퇴
 function* deleteUserSaga() {
-  if (window.confirm('정말로 회원탈퇴 하시겠습니까?')) {
-    try{
-      const response = yield call(deleteApi)
+  if (window.confirm("정말로 회원탈퇴 하시겠습니까?")) {
+    try {
+      const response = yield call(deleteApi);
       if (response.status === 200) {
-        yield put(logoutAsync())
-        yield alert('회원탈퇴되었습니다')
-        yield window.location.replace('/')
+        yield put(logoutAsync());
+        yield alert("회원탈퇴되었습니다");
+        yield window.location.replace("/");
       }
     } catch (error) {
-      alert(error.response.data.message)
+      alert(error.response.data.message);
     }
   } else {
-    alert('당신은 방금 털보를 배신할 뻔 했습니다.')
+    alert("당신은 방금 털보를 배신할 뻔 했습니다.");
   }
 }
 // 회원탈퇴 끝
 
 // 지갑 정보 가져오기
 function* fetchWalletSaga() {
-  try{
-    const response = yield call(fetchWalletApi)
+  try {
+    const response = yield call(fetchWalletApi);
     if (response.status === 200) {
-      yield put(fetchWallet(response.data))
+      yield put(fetchWallet(response.data));
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 // 지갑 정보 가져오기 끝
@@ -146,9 +145,9 @@ export function* accountSaga() {
   yield takeLatest(LOGIN_ASYNC, loginSaga); // 가장 마지막으로 디스패치된 DECREASE_ASYNC 액션만을 처리
   yield takeLatest(SIGNUP_ASYNC, signupSaga); // 가장 마지막으로 디스패치된 DECREASE_ASYNC 액션만을 처리
   yield takeLatest(FETCH_USER_ASYNC, fetchUserSaga); // 가장 마지막으로 디스패치된 FETCH_USER_ASYNC 액션만을 처리
-  yield takeLatest(CATCH_LOGIN, catchLoginSaga)
-  yield takeLatest(LOGOUT_ASYNC, logoutSaga)
-  yield takeLatest(PUT_USER_ASYNC, putUserSaga) // 유저정보 수정
-  yield takeLatest(DELETE_USER_ASYNC, deleteUserSaga) // 회원탈퇴
-  yield takeLatest(FETCH_WALLET_ASYNC, fetchWalletSaga) // 회원탈퇴
+  yield takeLatest(CATCH_LOGIN, catchLoginSaga);
+  yield takeLatest(LOGOUT_ASYNC, logoutSaga);
+  yield takeLatest(PUT_USER_ASYNC, putUserSaga); // 유저정보 수정
+  yield takeLatest(DELETE_USER_ASYNC, deleteUserSaga); // 회원탈퇴
+  yield takeLatest(FETCH_WALLET_ASYNC, fetchWalletSaga); // 회원탈퇴
 }
