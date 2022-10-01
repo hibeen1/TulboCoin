ed99da297b264c6faf57598d04e61168eb8e6abd
SSAFY <dkshktpq@naver.com>
2022-10-02 02:39:47 +0900

    fix: 회원가입로직 수정
    회원가입 후 바로 로그인 되도록 로직 수정

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -8,10 +8,9 @@ import { coinSaga } from "./coinSaga";
 
 let BASE_URL = "https://j7e203.p.ssafy.io/api/";
 
-if (process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:8080/api/";
-  // BASE_URL = "http://localhost:3000/";
-}
+// if (process.env.NODE_ENV === "development") {
+//   BASE_URL = "http://localhost:8080/api/";
+// }
 
 const DEFAULT_ACCEPT_TYPE = "application/json";
 
