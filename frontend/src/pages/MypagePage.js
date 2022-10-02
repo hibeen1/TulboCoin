716cc38e7f562fcd52835dc88a8cef5afa70720e
DESKTOP-KVCQHCD\SSAFY <kjasm0714@naver.com>
2022-10-02 22:09:08 +0900

    design : myPage 디자인 수정 중
    my page 디자인 수정 중

--- frontend/src/pages/MypagePage.js
+++ frontend/src/pages/MypagePage.js
@@ -4,7 +4,13 @@ import { putUserAsync, deleteUserAsync, resetWalletAsync } from '../store/accoun
 import MyWallet from "../components/MyWallet"
 import styled from "styled-components"
 import Navbar from "../components/Navbar"
+import GreySetting from "../media/images/icons/GreySetting.png"
+import BlueSetting from "../media/images/icons/BlueSetting.png"
+import GreyRefresh from "../media/images/icons/GreyRefresh.png"
+import BlueRefresh from "../media/images/icons/BlueRefresh.png"
 
+
+import PiggyBank from "../media/images/PiggyBank.png"
 const MyPageBlock = styled.div`
   display: flex;
 `
@@ -25,22 +31,120 @@ background-color: #F3F3F3;
   height: 100vh;
   position: relative;
   display: flex;
+  flex-direction: column;
 `
 
 const ProfileBlock = styled.div`
 /* background-color: #F3F3F3; */
-  border: solid red 3px;
-  width: 94vw;
+  border: solid green 3px;
+  width: 91vw;
   height: 30vh;
+  margin-left: 1vw;
+  margin-top: 1vw;
+
+  flex-direction: column;
+
+  h1 {
+    /* border: solid black 3px; */
+  }
+
+  p{
+    /* border: solid purple 3px;  */
+  }
+
+  .email{
+    margin-left: 40vw;
+
+  }
 
 `
 
 const StyledImg = styled.img`
   width: 3vw;
-  height: 3vh;
+  height: 5vh;
+  position:fixed;
+  margin-top: 1vh;
+  margin-left: 56vw;
+  display:inline;
+  /* border: 3px red solid; */
 `
   
+const WalletBlock = styled.div`
+  border: solid red 3px;
+  width: 85vw;
+  height: 30vh;
+  margin-left: 1vw;
+  margin-top: 5vw;
 
+  
+`
+// 회원정보 수정하기 버튼
+const SettingButton = styled.button`
+  width: 1.5vw;
+  height: 3vh;
+  background: url(${GreySetting}) no-repeat center;
+  background-size: 1.5vw 3vh;
+  margin-left: 3vw;
+  margin-top: 2vh;
+  display:inline;
+  position:fixed;
+  /* border: 3px black solid; */
+  :hover {
+      background: url(${BlueSetting}) center no-repeat;
+      background-size: 1.5vw 3vh;
+    }
+  
+`
+
+// 잔액 표시된 하얀 네모
+const CashBlock = styled.div`
+  background-color: #ffffff;
+  width: 30vw;
+  height: 20vh;
+  margin-top: 5vh;
+  flex-direction: row;
+  display:flex;
+  border: 0.911773px solid #E7E8F2;
+  border-radius: 5.47064px;
+`
+const PiggyBankImg = styled.div`
+  width: 6vw;
+  height: 8.5vh;
+  background: url(${PiggyBank}) no-repeat center;
+  background-size: 6vw 8.5vh;
+  margin-top: 2vh;
+  margin-left: 1vw;
+  
+`
+
+const BalanceRefreshBtn = styled.button`
+  width: 1.5vw;
+  height: 3vh;
+  background: url(${GreyRefresh}) no-repeat center;
+  background-size: 1.5vw 3vh;
+  margin-left: 3vw;
+  margin-top: 2vh;
+  display:inline;
+  position:fixed;
+  /* border: 3px black solid; */
+  :hover {
+      background: url(${BlueRefresh}) center no-repeat;
+      background-size: 1.5vw 3vh;
+    }
+  
+`
+
+const GraphBlock = styled.div`
+  background-color: #ffffff;
+  width: 30vw;
+  height: 20vh;
+  margin-top: 5vh;
+  flex-direction: row;
+  display:flex;
+  border: 0.911773px solid #E7E8F2;
+  border-radius: 5.47064px;
+  
+`
 
 function MypagePage() {
   const dispatch = useDispatch()
@@ -130,16 +234,31 @@ function MypagePage() {
       :
       <ProfileBlock>
         <div>
-          <h1 style="display:inline;">좋은 하루 되세요 {user.userId}님!</h1>
-          <p><StyledImg src={`${process.env.PUBLIC_URL}/profile/profile${user.imagePath}.png`} alt={`프로필 이미지${user.imagePath}`} /></p>
-          <p>이메일 : {user.email}</p>
-          <p>잔액 : {user.balance} KRW</p>
-          <button onClick={handleBalanceReset}>잔액 초기화하기</button>
+          <h1 style={{display:'inline'}}>좋은 하루 되세요 {user.userId}님!</h1>
+           <StyledImg src={`${process.env.PUBLIC_URL}/profile/profile${user.imagePath}.png`} alt={`프로필 이미지${user.imagePath}`} />
+          <p style={{display:'inline'}} className='email'>{user.email}</p>
+          {/* 회원정보 수정하기 버튼 */}
+          <SettingButton onClick={handlePageToForm}></SettingButton>
+          <h3>잔고</h3>
+          <div style={{display:'inline'}}>
+          <CashBlock>
+            <PiggyBankImg></PiggyBankImg>
+          <div><p>잔액 : {user.balance} 원</p></div>
+          <div><BalanceRefreshBtn onClick={handleBalanceReset}>잔액 초기화하기</BalanceRefreshBtn></div>
+          </CashBlock>
+          </div>
+
+          <GraphBlock>
+            {/* 원그래프 들어올 자리 */}
+          </GraphBlock>
         </div>
-        <button onClick={handlePageToForm}>수정하기</button>
-        <p>나의 보유 코인</p>
+        
+        <br />
+          <WalletBlock>
+          <p>나의 보유 코인</p>
         <hr />
-        <MyWallet />
+            <MyWallet />  
+          </WalletBlock>
         </ProfileBlock>
 
     }
