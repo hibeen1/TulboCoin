f893ae11991b96e1d160695bf30dc05daedeed4e
SSAFY <qownstlr0394@naver.com>
2022-10-05 00:33:45 +0900

    test : test

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,10 @@
-import styled from "styled-components"
-import Navbar from "../components/Navbar"
-import WordCloud from "../utils/WordCloud"
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import WordCloud from "../utils/WordCloud";
 
 const HomePageBlock = styled.div`
   display: flex;
-`
+`;
 
 const NavBlock = styled.div`
   /* border: solid yellow 3px; */
@@ -12,30 +12,29 @@ const NavBlock = styled.div`
   height: 100vh;
   position: relative;
   display: flex;
-  
-`
+`;
 
 const HomeBlock = styled.div`
-background-color: #F3F3F3;
+  background-color: #f3f3f3;
   border: solid black 3px;
   width: 94vw;
   height: 300vh;
   position: relative;
   /* display: flex; */
-`
+`;
 
 function HomePage() {
-  return(
-  <HomePageBlock>
-    <NavBlock>
-      <Navbar></Navbar>
-    </NavBlock>
-    <HomeBlock>
-    {/* <h1>워드클라우드 및 소개 페이지입니다</h1> */}
-    <WordCloud></WordCloud>
-    </HomeBlock>
-  </HomePageBlock>
-  )
+  return (
+    <HomePageBlock>
+      <NavBlock>
+        <Navbar></Navbar>
+      </NavBlock>
+      <HomeBlock>
+        {/* <h1>워드클라우드 및 소개 페이지입니다</h1> */}
+        <WordCloud></WordCloud>
+      </HomeBlock>
+    </HomePageBlock>
+  );
 }
 
 export default HomePage;
