b593db65b4e0c3ef0f8bbe81ab7ab52e8d1b482a
hibeen1 <hibeen1@naver.com>
2022-10-04 16:23:49 +0900

    docs : Spark api에 Swagger 추가

--- backend/src/main/java/bigdataproject/backend/api/controller/SparkController.java
+++ backend/src/main/java/bigdataproject/backend/api/controller/SparkController.java
@@ -1,6 +1,7 @@
 package bigdataproject.backend.api.controller;
 
 import bigdataproject.backend.api.service.SparkService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class SparkController {
     SparkService sparkService;
 
     @GetMapping
+    @ApiOperation(value = "지난 시간 거래 트래픽 조회", notes = "input : int (ex. 10, 30, 60) output : Map 형식 {\"KRW-BTC\" : 29, ...}")
     public Map<String, Long> count(@RequestParam(required = true) int time) {
         LocalDateTime ldt = LocalDateTime.now(ZoneId.of("UTC")).minusMinutes(time);
         LocalDateTime now = LocalDateTime.now();
