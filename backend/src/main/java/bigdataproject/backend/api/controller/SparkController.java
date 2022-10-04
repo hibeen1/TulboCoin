c133c5206c4a46c435ba585f78e60fa13e0427cb
bds01088 <bds01088@naver.com>
2022-10-05 02:24:43 +0900

    refactor : wordCount 응답 변경

--- backend/src/main/java/bigdataproject/backend/api/controller/SparkController.java
+++ backend/src/main/java/bigdataproject/backend/api/controller/SparkController.java
@@ -1,19 +1,18 @@
 package bigdataproject.backend.api.controller;
 
 import bigdataproject.backend.api.service.SparkService;
+import bigdataproject.backend.db.WebClient.WordCountDTO;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.Map;
+import java.util.List;
 
 @RestController
 @RequestMapping("/spark")
+@CrossOrigin
 public class SparkController {
 
     @Autowired
@@ -21,7 +20,7 @@ public class SparkController {
 
     @GetMapping
     @ApiOperation(value = "지난 시간 거래 트래픽 조회", notes = "input : int (ex. 10, 30, 60) output : Map 형식 {\"KRW-BTC\" : 29, ...}")
-    public Map<String, Long> count(@RequestParam(required = true) int time) {
+    public List<WordCountDTO> count(@RequestParam(required = true) int time) {
         LocalDateTime ldt = LocalDateTime.now(ZoneId.of("UTC")).minusMinutes(time);
         LocalDateTime now = LocalDateTime.now();
         return sparkService.getCount(ldt, now, time/10);
--- backend/src/main/java/bigdataproject/backend/api/service/SparkServiceImpl.java
+++ backend/src/main/java/bigdataproject/backend/api/service/SparkServiceImpl.java
@@ -1,6 +1,6 @@
 package bigdataproject.backend.api.service;
 
-import bigdataproject.backend.db.entity.Coin;
+import bigdataproject.backend.db.WebClient.WordCountDTO;
 import bigdataproject.backend.db.repository.CoinRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,8 +9,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @Slf4j
@@ -22,7 +21,7 @@ public class SparkServiceImpl implements SparkService {
     private final CoinRepository coinRepository;
 
     @Override
-    public Map<String, Long> getCount(LocalDateTime ldt, LocalDateTime now, int cnt) {
+    public List<WordCountDTO> getCount(LocalDateTime ldt, LocalDateTime now, int cnt) {
         Map<String, Long> wordCounts = new HashMap<>();
         Map<String, Long> coinNameCounts = new HashMap<>();
         JavaRDD<String> coinMap = null;
@@ -35,16 +34,21 @@ public class SparkServiceImpl implements SparkService {
                 System.out.println("비교" + ldt);
                 System.out.println("현재" + now);
                 wordCounts = coinMap.countByValue();
+                List<WordCountDTO> countDTOList = new ArrayList<>();
+
                 System.out.println("너 얼마나 돌았니??  " + testCnt);
                 for (Map.Entry<String, Long> entry : wordCounts.entrySet()){
                     try {
-                        coinNameCounts.put(coinRepository.findCoinByCoinCode(entry.getKey()).getCoinName(), entry.getValue());
+                        WordCountDTO wordCountDTO = new WordCountDTO();
+                        wordCountDTO.setValue(coinRepository.findCoinByCoinCode(entry.getKey()).getCoinName());
+                        wordCountDTO.setCount(entry.getValue());
+                        countDTOList.add(wordCountDTO);
                     } catch (NullPointerException nullPointerException){
                         log.info("Coin 테이블에 존재하지않는 코인이 워드 카운트에 존재함");
                     }
                 }
-//                log.info(coinNameCounts.toString());
-                return coinNameCounts;
+                log.info(countDTOList.toString());
+                return countDTOList;
             }
             testCnt += 1;
 
